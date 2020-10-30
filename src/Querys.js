3d4fc9cebfe97e8aa8e6d543c1cf3516cc6aacfc
github_galvezgarciajorge <jgalvez@iespuntadelverde.es>
2020-10-30 03:28:54 +0100

    first commit

---
+++ src/Querys.js
@@ -0,0 +1,10 @@
+
+
+
+db.querysbd.insertMany([
+  { item: "macbookpro",qty: 25,size:{h:1.56, w:30.4, oum: "cm"},status: "A"},
+  { item: "macbookair", qty: 80, size:{ h:0.41, w:30.4, oum: "cm"}, status: "A"},
+  { item: "macbookprob", qty: 100, size:{ h:1.62, w:35.79, oum: "cm"}, status: "A"},
+  { item: "ipad", qty: 200, size:{ h:11, oum: "in"}, status: "B"},
+  { item: "ipadpro", qty: 150, size:{ h:12.9, oum: "in"}, status: "B"},
+])
